2ba249fa2c02f38f3e238ff6895bef3226cd4ff4
Sky2walkerss <starw8169@gmail.com>
2022-05-04 18:19:09 +0300

    first commit

--- SweetShop/urls.py
+++ SweetShop/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'^cart/', include(('cart.urls', 'cart'),namespace='cart')),
+    path(r'^orders/', include(('orders.urls','orders'), namespace='orders')),
     path(r'', include(('shop.urls', 'shop'), namespace='shop')),
     path(r'store', include(('shop.urls', 'shop'), namespace='shop')),
     path(r'news', include(('shop.urls', 'shop'), namespace='shop')),
